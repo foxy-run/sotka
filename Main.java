78041cca1b63a9e3edacb5e524b0ed7a2ee07070
Xenia Bocharova <bochxenia@yandex.ru>
2020-08-20 22:17:11 +0300

    Добавлен код

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        int score = 375;
+        int refill = 2555;
+        int total = score + refill;
+
+if (refill > 1000){
+    int bonus = refill / 100;
+    System.out.println(bonus);
+    System.out.println(total + bonus);
+}
+else {
+    System.out.println(total);
+    }
+
+    }
+}
